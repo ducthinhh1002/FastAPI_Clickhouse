8e60c8f5526787268adec77441d68ca6336b8d55
ducthinh1002 <thinh.nd210817@sis.hust.edu.vn>
2025-07-30 08:51:02 +0700

    password

--- app/core/config.py
+++ app/core/config.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     CLICKHOUSE_HOST: str = "clickhouse"
     CLICKHOUSE_PORT: int = 9000
-    CLICKHOUSE_USER: str = "myuser"
-    CLICKHOUSE_PASSWORD: str = "my_password"
+    CLICKHOUSE_USER: str = "admin"
+    CLICKHOUSE_PASSWORD: str = "password"
     CLICKHOUSE_DATABASE: str = "default"
 
 settings = Settings()
